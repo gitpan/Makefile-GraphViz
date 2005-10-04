92c65eb26c87a54a45c406381658da39f1717737
Yichun Zhang (章亦春) <agentzh@gmail.com>
2005-10-04 10:34:45 +0000

    Import of AGENT/Makefile-GraphViz-0.02 from CPAN.
    gitpan-cpan-distribution: Makefile-GraphViz
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         AGENT/Makefile-GraphViz-0.02.zip
    gitpan-cpan-author:       AGENT
    gitpan-cpan-maturity:     released

--- lib/Makefile/GraphViz.pm
+++ lib/Makefile/GraphViz.pm
@@ -1,9 +1,9 @@
 #: Makefile/GraphViz.pm
 #: Plot the detailed structure of Makefiles
 #:   using GraphViz
-#: v0.01
+#: v0.02
 #: Copyright (c) 2005 Agent Zhang
-#: 2005-09-30 2005-10-01
+#: 2005-09-30 2005-10-04
 
 package Makefile::GraphViz;
 
@@ -13,41 +13,94 @@ use warnings;
 use GraphViz;
 use base 'Makefile::Parser';
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
-my $node_attr =
-{
+our $IDCounter = 0;
+
+my %VirNodeStyle =
+(
+    shape => 'box',
+    style => 'dotted',
+    fillcolor => 'yellow',
+);
+
+my %NormalNodeStyle =
+(
     shape => 'box',
     style => 'filled',
     fillcolor => 'yellow',
-};
+);
 
-my $edge_attr =
-{
+my %EdgeStyle =
+(
     color => 'red',
-};
+);
 
-my %args = (
+my %CmdStyle =
+(
+    shape => 'ellipse',
+    style => 'filled',
+    fillcolor => 'gray',
+);
+
+my %InitArgs = (
     layout => 'dot',
     ratio => 'auto',
-    node => $node_attr,
-    edge => $edge_attr,
+    node => \%NormalNodeStyle,
+    edge => \%EdgeStyle,
 );
 
 our %Nodes;
 
 sub plot {
-    my ($self, $root_name, $gv) = @_;
+    my $self = shift;
+    my $root_name = shift;
+    my %opts = @_;
+    #warn "@_\n";
+
+    # process the ``gv'' option:
+    my $gv = $opts{gv};
+
+    # process the ``vir_tars'' option:
+    my $val = $opts{vir_tars};
+    my %vir_tars = %$val if $val and ref $val;
+
+    # process the ``init_args'' option:
+    $val = $opts{init_args};
+    my %init_args = ($val and ref $val) ? %$val : %InitArgs;
+
+    # process the ``edge_style'' option:
+    $val = $opts{edge_style};
+    my %edge_style = ($val and ref $val) ? %$val : %EdgeStyle;
+    $init_args{edge} = \%edge_style;
+
+    # process the ``normal_node_style'' option:
+    $val = $opts{normal_node_style};
+    my %normal_node_style = ($val and ref $val) ? %$val : %NormalNodeStyle;
+    $init_args{node} = \%normal_node_style;
+
+    # process the ``vir_node_style'' option:
+    $val = $opts{vir_node_style};
+    my %vir_node_style = ($val and ref $val) ? %$val : %VirNodeStyle;
+
+    # process the ``cmd_style'' option:
+    $val = $opts{cmd_style};
+    my %cmd_style = ($val and ref $val) ? %$val : %CmdStyle;
+
+    # process the ``trim_mode'' option:
+    my $trim_mode = $opts{trim_mode};
+    #warn "TRIM MODE: $trim_mode\n";
+
     my $root = ($root_name and ref $root_name) ? $root_name : ($self->target($root_name));
 
     if (!$gv) {
-        $gv = GraphViz->new(%args);
+        $gv = GraphViz->new(%init_args);
         %Nodes = ();
     }
     #warn $gv;
     if (!$Nodes{$root_name}) {
-        (my $short_name = $root_name) =~ s/.*(.{5}[\\\/].*)$/...$1/o;
-        $short_name =~ s/\\/\//g;
+        my $short_name = trim_path($root_name);
+        #$short_name =~ s/\\/\//g;
         #warn $short_name, "\n";
         $gv->add_node($root_name, label => $short_name);
         $Nodes{$root_name} = 1;
@@ -57,15 +110,44 @@ sub plot {
     #warn "GraphViz: $gv\n";
     return $gv if !$root;
 
+    my $lower_node;
+    my @cmds = $root->commands;
+    if (!$trim_mode and @cmds) {
+        $lower_node = gen_id();
+        my $cmds = join("\n", map { trim_cmd($_); } @cmds);
+        $gv->add_node($lower_node, label => $cmds, %cmd_style);
+        $gv->add_edge($lower_node => $root_name);
+    } else {
+        $lower_node = $root_name;
+    }
+
     my @depends = $root->depends;
     foreach (@depends) {
         #warn "$_\n";
-        $gv->add_edge($_ => $root_name);
-        $self->plot($_, $gv);
+        $gv->add_edge($_ => $lower_node);
+        $self->plot($_, gv => $gv, @_);
     }
     #warn "END\n";
     #warn "GraphViz: $gv\n";
     return $gv;
+}
+
+sub gen_id {
+    return ++$IDCounter;
+}
+
+sub trim_path {
+    my $s = shift;
+    $s =~ s/.+(.{5}[\\\/].*)$/...$1/o;
+    $s =~ s/\\/\\\\/g;
+    return $s;
+}
+
+sub trim_cmd {
+    my $s = shift;
+    $s =~ s/((?:\S+\s*){1,2}).+/$1/o;
+    $s =~ s/\\/\\\\/g;
+    return $s;
 }
 
 1;
@@ -110,6 +192,41 @@ This stuff is highly experimental and is currently at B<ALPHA> stage, so
 production use is strongly discouraged. Anyway, I have the plan to 
 improve this stuff unfailingly.
 
+=head1 SAMPLES
+
+Given the following Makefile definition:
+
+    BASE = pat_cover
+    EXE = $(BASE).ast
+
+    t\$(EXE).asm : bin2asm.pl t\$(EXE).bin
+        perl bin2asm.pl t\$(EXE).bin > $@
+
+    t\$(EXE).bin : hex2bin.exe t\$(EXE).hex
+        hex2bin.exe t\$(EXE).hex $@
+
+    t\$(EXE).hex : ast2hex.pl $(BASE).ast
+        perl ast2hex.pl $(BASE).ast > $@
+
+    $(BASE).ast : pat_cover
+
+    pat_cover :
+        perl $(BASE).pl $(RAW_AST) $(GLOB_AST) > $(BASE).ast
+
+    hex2bin.exe : hex2bin.c
+        cl /nologo /O2 hex2bin.c
+
+We can get the following PNG output (You won't see the picture unless you
+used HTML to render this POD doc):
+
+=begin html
+
+<center>
+    <img src="doc.png" alt="Stub Picture for the PNG output"/>
+</center>
+
+=end html
+
 =head1 The Makefile::GraphViz Class
 
 This class is a subclass inherited from L<Makefile::Parser>. So all the methods (and
@@ -136,6 +253,26 @@ given target can't be found in Makefile, the target will be plotted separately.
 
 None by default.
 
+=head2 INTERNAL FUNCTIONS
+
+Internal functions should not be used directly.
+
+=over
+
+=item gen_id
+
+Generate a unique id for command node.
+
+=item trim_path
+
+Trim the path to a more readable form.
+
+=item trim_cmd
+
+Trim the shell command to a more friendly size.
+
+=back
+
 =head1 CODE COVERAGE
 
 I use L<Devel::Cover> to test the code coverage of my tests, below is the 
@@ -144,8 +281,8 @@ L<Devel::Cover> report on this module test suite.
     ---------------------------- ------ ------ ------ ------ ------ ------ ------
     File                           stmt   bran   cond    sub    pod   time  total
     ---------------------------- ------ ------ ------ ------ ------ ------ ------
-    .../lib/Makefile/GraphViz.pm  100.0   87.5   33.3  100.0  100.0  100.0   93.5
-    Total                         100.0   87.5   33.3  100.0  100.0  100.0   93.5
+    .../lib/Makefile/GraphViz.pm  100.0   68.2   41.7  100.0  100.0  100.0   82.8
+    Total                         100.0   68.2   41.7  100.0  100.0  100.0   82.8
     ---------------------------- ------ ------ ------ ------ ------ ------ ------
 
 =head1 SEE ALSO
--- t/Makefile-GraphViz.t
+++ t/Makefile-GraphViz.t
@@ -1,18 +1,37 @@
-use Test::More tests => 8;
+#: Makefile-GraphViz.t
+#: Test Makefile::GraphViz
+#: Copyright (c) 2005 Agent Zhang
+#: v0.02
+#: 2005-09-30 2005-10-04
+
+use Test::More tests => 14;
 use Makefile::GraphViz;
 use File::Compare;
 
 my $parser = Makefile::GraphViz->new;
 ok $parser;
 isa_ok $parser, 'Makefile::GraphViz';
-ok $parser->parse('Makefile');
-ok $parser->{_file};
+ok $parser->parse("t/Makefile");
+is $parser->{_file}, 't/Makefile';
 
 # plot the tree rooted at the install target in Makefile:
-my $gv = $parser->plot('install');
+#warn "Target: ", $parser->target('t\pat_cover.ast.asm');
+my $gv = $parser->plot('t\\pat_cover.ast.asm');
 ok $gv;
 isa_ok $gv, 'GraphViz';
-my $png = 't/install.png';
+my $png = 't/doc.png';
 ok $gv->as_png($png);
-is File::Compare::compare($png, "$png~"), 0;
+is File::Compare::compare($png, "$png.png"), 0;
+unlink $png;
+
+ok $parser->parse("t/Makefile2");
+is $parser->{_file}, 't/Makefile2';
+
+# plot the tree rooted at the install target in Makefile:
+$gv = $parser->plot('install', trim_mode => 1);
+ok $gv;
+isa_ok $gv, 'GraphViz';
+$png = 't/install.png';
+ok $gv->as_png($png);
+is File::Compare::compare($png, "$png.png"), 0;
 unlink $png;
